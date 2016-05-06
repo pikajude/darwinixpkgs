ea6d93baedb3a071e40c9a43f18e7847afbd00a9
Jude Taylor <me@jude.bio>
2016-05-05 21:12:28 -0700

    patch xcode detection out of all gyp versions

--- pkgs/development/web/nodejs/nodejs.nix
+++ pkgs/development/web/nodejs/nodejs.nix
@@ -37,6 +37,7 @@ in stdenv.mkDerivation {
   dontDisableStatic = true;
   prePatch = ''
     patchShebangs .
+    sed -i 's/raise.*No Xcode or CLT version detected.*/version = "7.0.0"/' deps/npm/node_modules/node-gyp/gyp/pylib/gyp/xcode_emulation.py
     sed -i 's/raise.*No Xcode or CLT version detected.*/version = "7.0.0"/' tools/gyp/pylib/gyp/xcode_emulation.py
   '';
 
