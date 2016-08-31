95bc0f38871eddd8d66302746c0855082d9c8474
Jude Taylor <me@jude.bio>
2016-08-31 15:30:18 -0700

    fix atomicparsley build

--- pkgs/tools/video/atomicparsley/default.nix
+++ pkgs/tools/video/atomicparsley/default.nix
@@ -10,18 +10,23 @@ stdenv.mkDerivation rec {
     sha256 = "de83f219f95e6fe59099b277e3ced86f0430ad9468e845783092821dff15a72e";
   };
 
-  buildInputs = with pkgs; [ unzip ]
-    ++ stdenv.lib.optional stdenv.isDarwin [ darwin.apple_sdk.frameworks.Cocoa ];
+  buildInputs = with pkgs; [ unzip ];
   patches = [ ./casts.patch ];
   setSourceRoot = "sourceRoot=${product}-source-${version}";
   buildPhase = "bash build";
   installPhase = "install -D AtomicParsley $out/bin/AtomicParsley";
 
+  frameworks = [ "Cocoa" ];
+
   postPatch = ''
     substituteInPlace build \
-      --replace 'g++' 'c++'
+      --replace g++ c++
     substituteInPlace AP_NSImage.mm \
-      --replace '_NSBitmapImageFileType' 'NSBitmapImageFileType'
+      --replace _NSBitmapImageFileType NSBitmapImageFileType \
+      --replace 'char* suffix' 'const char* suffix'
+    substituteInPlace AP_NSFile_utils.mm \
+      --replace 'char* output_suffix' 'const char* output_suffix' \
+      --replace 'char* input_suffix' 'const char* input_suffix'
   '';
 
   meta = with stdenv.lib; {
